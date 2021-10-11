62a9094216c1131c7deee0003702dea43b385ed7
Budi <ignacio.tirabasso@gmail.com>
2021-10-11 13:44:27 +0100

    256 colors palette

--- contracts/BMPGenerator.sol
+++ contracts/BMPGenerator.sol
@@ -6,7 +6,9 @@ import "hardhat/console.sol";
 contract BMPGenerator {
     string private greeting;
 
-    uint24[16][] private _palettes;
+
+    // uint24[16][] private _palettes;
+    uint24[256][] private _palettes;
     uint256[32][] private _templates;
 
     function getBMP(uint256 templateId, uint256 paletteId)
@@ -16,7 +18,7 @@ contract BMPGenerator {
     {
         uint256 gasBefore = gasleft();
         uint256[32] storage template = getTemplate(templateId);
-        uint24[16] storage palette = getPalette(paletteId);
+        uint24[256] storage palette = getPalette(paletteId);
         bmp = generateBMP(template, palette);
 
         uint256 gasAfter = gasleft();
@@ -24,7 +26,7 @@ contract BMPGenerator {
     }
 
     // todo : support rgba
-    function generateBMP(uint256[32] memory template, uint24[16] memory palette)
+    function generateBMP(uint256[32] memory template, uint24[256] memory palette)
         public
         pure
         returns (uint24[32 * 32] memory bmp)
@@ -46,14 +48,13 @@ contract BMPGenerator {
         }
     }
 
-    // pack it
     function getTemplate(uint256 templateId)
         internal
         view
         returns (uint256[32] storage template)
     {
         if (templateId == 0) {
-            templateId =  _templates.length-1;
+            templateId =  _templates.length - 1;
         }
         template = _templates[templateId];
     }
@@ -61,23 +62,20 @@ contract BMPGenerator {
     function getPalette(uint256 paletteId)
         internal
         view
-        returns (uint24[16] storage)
+        returns (uint24[256] storage)
     {
         if (paletteId == 0) {
             paletteId =  _palettes.length-1;
         }
-        uint24[16] storage palette = _palettes[paletteId];
+        uint24[256] storage palette = _palettes[paletteId];
         return palette;
     }
 
-    // counters
-
     function addTemplate(uint256[32] memory newTemplate) external {
-        // read evm of this line
         _templates.push(newTemplate);
     }
 
-    function addPalette(uint24[16] memory palette) external {
+    function addPalette(uint24[256] memory palette) external {
         _palettes.push(palette);
     }
 }
